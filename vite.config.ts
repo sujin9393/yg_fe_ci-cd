327a4607696fa2433993f90c36ce2c50b29f9dcf
sujinlee <soongjing@sujinlees-MacBook-Pro.local>
2025-05-08 11:12:03 +0900

    Fix vite.config.ts syntax

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,6 @@
-//import { defineConfig } from 'vite'
-//import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-//export default defineConfig({
-  plugins: [react()],
-//})
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  },
-  base: '/', // 혹시 nginx에서 서브디렉토리로 라우팅할 경우 base 경로 조정
-})
+});
