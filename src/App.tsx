024e34d39d93b02244949e47418f7bca062c9502
sujinlee <soongjing@sujinlees-MacBook-Pro.local>
2025-05-08 11:04:50 +0900

    FE 테스트용 문구 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <S.MainContainer>
       <Header />
-      <div>💡 이것은 테스트입니다</div>  {/* 이 줄 추가 */}
+      <div>💡 이것은 테스트입니다</div> 
     </S.MainContainer>
   );
 };
